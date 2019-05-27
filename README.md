ec3d45e2924ed72814c6c9f81dccaac6a90c981b
Junhao Su <jusu@ucsc.edu>
2019-05-26 19:43:54 -0700

    CMPM163HW3

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# CMPM163HW3partA
+# This is the assignment for CMPM163HW3partA
+# I used particle effect to generate the blue fire.
+# I used noise function to created a shpere with adjustable roughness value.
+# I also implemented fire burn sound effect.
---
+++ Scripts/NoiseSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class NoiseSettings
+{
+
+    public float strength = 1f;
+    public float roughness = 2.5f;
+    public Vector3 centre;
+}
